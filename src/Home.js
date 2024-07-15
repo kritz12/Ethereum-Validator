e2e92119689c274bbefa4f6b5543dcd7a7b469f2
kritz12 <83504885+kritz12@users.noreply.github.com>
2024-07-15 22:39:47 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -8,11 +8,14 @@ function Home() {
         <img src="ethereum.png" alt="Your Name" className="profile-image" />
       </div>
       <div className="details-container">
-        <h1>Welcome to My Project</h1>
+        <h1>Welcome to Ethereum Validator App</h1>
+        <p>Ethereum is a decentralized blockchain platform that enables a wide range of applications,
+           including decentralized finance (DeFi) and non-fungible tokens (NFTs), and is continually 
+           evolving to improve scalability and security.</p>
         <p>This React-based web app tracks Ethereum validators using
            data from the beaconcha.in API. It includes a Navbar for easy 
-          navigation to the welcome message and validator search.
-          The Sidebar lists the top 10 validators with links to their detailed stats.
+          navigation to the welcome message and validator search.</p>
+         <p>The Sidebar lists the top 10 validators with links to their detailed stats.
           Users can input a validator ID or pubkey to view specific validator details,
          such as statistics and attestations. The app aims for user-friendly
          navigation and clear presentation of Ethereum validator information.</p>
